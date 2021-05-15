a658e25afefa4385331fcc5771b0bcbe960c8339
Andrew Huntington <andrew.huntington@gmail.com>
2021-05-15 16:12:16 +0900

    Add image saving, improve styling

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
+
+const { handleErrors } = require("./middlewares");
 const usersRepo = require("../../repositories/users");
 const signupTemplate = require("../../views/admin/auth/signup");
 const signinTemplate = require("../../views/admin/auth/signin");
@@ -21,16 +22,9 @@ router.get("/signup", (req, res) => {
 router.post(
   "/signup",
   [requireEmail, requirePassword, requirePasswordConfirmation],
+  handleErrors(signupTemplate),
   async (req, res) => {
-    // results from validation (above array) are attached to the req obj
-    const errors = validationResult(req);
-
-    // isEmpty() is a part of express-validator
-    if (!errors.isEmpty()) {
-      return res.send(signupTemplate({ req, errors }));
-    }
-
-    const { email, password, passwordConfirmation } = req.body;
+    const { email, password } = req.body;
 
     // Create a user in our user rep to represent this person
     const user = await usersRepo.create({ email, password });
@@ -54,13 +48,8 @@ router.get("/signin", (req, res) => {
 router.post(
   "/signin",
   [requireEmailExists, requireValidPasswordForUser],
+  handleErrors(signinTemplate),
   async (req, res) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      return res.send(signinTemplate({ errors }));
-    }
-
     const { email } = req.body;
 
     const user = await usersRepo.getOneBy({ email });
--- routes/admin/products.js
+++ routes/admin/products.js
@@ -1,13 +1,36 @@
+// Outside libraries
 const express = require("express");
+const multer = require("multer"); // handles multipart/form-data (file uploads)
+
+// Self-authored code
+const { handleErrors } = require("./middlewares");
 const productsRepo = require("../../repositories/products");
 const productsNewTemplate = require("../../views/admin/products/new");
+const { requireTitle, requirePrice } = require("./validators");
 
 const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
 
 router.get("/admin/products", (req, res) => {});
 
 router.get("/admin/products/new", (req, res) => {
   res.send(productsNewTemplate({}));
 });
+
+router.post(
+  "/admin/products/new",
+  upload.single("image"),
+  [requireTitle, requirePrice],
+  handleErrors(productsNewTemplate),
+  async (req, res) => {
+    // File data is saved as a buffer inside req.file object using Multer
+    // Convert that buffer to a string that is storable in a JSON file
+    const image = req.file.buffer.toString("base64");
+    const { title, price } = req.body;
+    await productsRepo.create({ title, price, image });
+
+    res.send("submitted");
+  }
+);
 
 module.exports = router;
